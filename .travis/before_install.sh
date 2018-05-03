7e9ebe4288e0d37058a650e7ff00fb0979549336
Lisa Qu <lisa.qu@veeva.com>
2018-05-03 13:22:02 -0700

    feat(test): test
    affects: demo-alpha, demo-beta, demo-usage
    
    tset
    
    BREAKING CHANGE:
    tes

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -8,12 +8,12 @@ echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/fruitCandy/l
 npm config set //registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN -q
 npm prune
 
+# email address to be recorded in any newly created commits
 git config --global user.email "pinkbear715@gmail.com"
+# full name to be recorded in any newly created commits
 git config --global user.name "Travis CI"
+# in centralized workflow, work like upstream with an added safety to refuse to push if the upstream branch’s name is different from the local one.
 git config --global push.default simple
-
-
-
 
 git fetch --tags
 git branch -u origin/$TRAVIS_BRANCH
