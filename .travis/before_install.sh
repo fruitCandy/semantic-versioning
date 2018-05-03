9deefe0ca433e2311cdd4a328687f37819b6777b
Lisa Qu <lisa.qu@veeva.com>
2018-05-03 10:26:32 -0700

    feat(test): t
    t
    
    BREAKING CHANGE:
    t
    
    ISSUES CLOSED: t

---
+++ .travis/before_install.sh
@@ -0,0 +1,21 @@
+MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
+git remote add origin git@github.com:fruitCandy/semantic-versioning.git
+git fetch origin
+git clone git@github.com:fruitCandy/semantic-versioning.git lerna-semantic-release
+git checkout $TRAVIS_BRANCH
+git config credential.helper store
+echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/atlassian/lerna-semantic-release.git" > ~/.git-credentials
+npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+npm prune
+
+git config --global user.email "pinkbear715@gmail.com"
+git config --global user.name "Fruit Candy"
+git config --global push.default simple
+
+git fetch --tags
+git branch -u origin/$TRAVIS_BRANCH
+git fsck --full
+echo "npm whoami"
+npm whoami #debug
+echo "git config --list"
+git config --list #debug
