f4692f2e97f1fb76acf870cb82748787188ad587
Lisa Qu <lisa.qu@veeva.com>
2018-05-03 11:27:19 -0700

    feat(yrdy): yrdy
    ydry
    
    BREAKING CHANGE:
    ydr
    
    ISSUES CLOSED: yd

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,12 +5,15 @@ MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 git checkout $TRAVIS_BRANCH
 git config credential.helper store
 echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/fruitCandy/lerna-semantic-release.git" > ~/.git-credentials
-npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+npm config set //registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN -q
 npm prune
 
 git config --global user.email "pinkbear715@gmail.com"
 git config --global user.name "Travis CI"
 git config --global push.default simple
+
+
+
 
 git fetch --tags
 git branch -u origin/$TRAVIS_BRANCH
