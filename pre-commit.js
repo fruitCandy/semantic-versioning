053b5bb030585273a0554d5e81d91834b4b73ba9
Lisa Qu <lisa.qu@veeva.com>
2018-05-29 13:42:58 -0700

    random

--- pre-commit.js
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env node
-
-echo "Can you make a commit? Well, it depends."
-exit 1
---
+++ prepush.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+echo $current_branch
+if [[ "$current_branch" =~ "master" ]]; then
+    echo 456
+    read -a array
+    pushed_ref=${array[1]}
+    emote_ref=${array[3]}
+
+    echo
+    echo "# Files changed:"
+    git diff --name-only $remote_ref $pushed_ref
+
+    files = $(git diff --name-only $remote_ref $pushed_ref)
+
+    echo $files
+    echo
+    read -p "You're about to push to production, are you sure? [y|n] " -r < /dev/tty
+    echo
+    if [[ $REPLY = 'y' ]]; then
+        echo yes
+        exit 1
+    else
+        echo no
+        exit 1
+    fi
+else
+    echo 789
+    exit 1
+fi
